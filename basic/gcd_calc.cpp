d7b89cd624533924eebcdda994778ddccd7b585b
Vijaya Manohar Dogiparthi <dvijaymanohar@gmail.com>
2023-05-10 06:43:34 +0200

    More practice code added

---
+++ basic/gcd_calc.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int gcd(int a, int b) {
+  while (b != 0) {
+    int temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+}
+
+int main() {
+  int num1, num2;
+  std::cout << "Enter two numbers: ";
+  std::cin >> num1 >> num2;
+  int result = gcd(num1, num2);
+  std::cout << "The gcd of " << num1 << " and " << num2 << " is " << result
+            << std::endl;
+  return 0;
+}
