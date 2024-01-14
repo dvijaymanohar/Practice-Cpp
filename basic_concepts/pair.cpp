57877c302f7e2f276a0195329e9fa7f962415c25
Vijaya Manohar Dogiparthi <dvijaymanohar@gmail.com>
2024-01-14 13:13:13 +0100

    Update pair.cpp

--- basic_concepts/pair.cpp
+++ basic_concepts/pair.cpp
@@ -7,5 +7,15 @@ int main(int argc, char *argvp[]) {
   std::cout << "Name of the employee: " << employee.first << std::endl;
   std::cout << "Id of the employee: " << employee.second << std::endl;
 
+
+  pair<const char*, double> charDoub("str", 3.14);
+  pair<const char*, double> charDoub2 = make_pair("str", 3.14);
+  auto charDoub3 = make_pair("str", 3.14);
+
+  cout << charDoub.first << ", " << charDoub.second << "\n";    // str, 3.14
+  charDoub.first = "Str";
+  get<1>(charDoub) = 4.14;
+  cout << charDoub.first << ", " << charDoub.second << "\n";    // Str, 4.14
+
   return 0;
 }
