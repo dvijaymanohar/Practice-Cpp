209a783dd64cac3f28d7ef6efec5850406e68594
Vijaya Manohar Dogiparthi <v.dogiparthi@rena.nl>
2024-01-14 13:08:39 +0100

    Update pair.cpp

--- basic_concepts/pair.cpp
+++ basic_concepts/pair.cpp
@@ -2,7 +2,10 @@
 #include <utility>
 
 int main(int argc, char *argvp[]) {
+  std::pair<std::string, int> employee("Vijay", 1);
 
+  std::cout << "Name of the employee: " << employee.first << std::endl;
+  std::cout << "Id of the employee: " << employee.second << std::endl;
 
-    return 0;
+  return 0;
 }
